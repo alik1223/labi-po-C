b1021a433c53e4afd85dbe9fd8ebc7a36b1119ab
alik1223 <43026754+alik1223@users.noreply.github.com>
2018-10-31 22:28:49 +0300

    Add files via upload

--- kursa4/main.c
+++ kursa4/main.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include <locale.h> //считывает текст на руском языке
 
+void main()
+{
+setlocale (LC_ALL, "");
+int a,b;
+FILE *fpin;
+fpin = fopen( "test.txt", "rt" );
+if( fpin == NULL )
+return;
+fscanf(fpin,"%d %d",&a,&b);
+int q=1;
+while (q!=0)
+    {
+        printf ("1 +\n");
+        printf ("2 -\n");
+        printf ("3 *\n");
+        printf ("4 /\n");
+        printf ("0 - vixod\n>");
+        scanf ("%d", &q);
+        switch (q)
+        {
+        case 1:
+            {
+                printf("%d\n",a+b);
+                break;
+            }
+
+        case 2:
+            {
+            printf("%d\n",a-b);
+            break;
+            }
+        case 3:
+            {
+            printf("%d\n",a*b);
+            break;
+            }
+        case 4:
+            {
+            printf("%d\n",a/b);
+            break;
+            }
+            case 0: break;
+            default: break;
+        }
+    }
+}
