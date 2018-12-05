758cf076fbebff49cff44d69994a0544da96a414
alik1223 <43026754+alik1223@users.noreply.github.com>
2018-12-05 21:53:20 +0300

    Add files via upload

--- лаба 11/main.c
+++ лаба 11/main.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#define MAXLINE 1024
+#include <windows.h>
+void  main ( void )
+{
+int c;
+FILE *f1;
+char *ptr;
+int s=0;
+char line[MAXLINE];
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+SetConsoleTextAttribute(hConsole, 15);
+f1 = fopen( "test.txt", "rt" );
+if( f1 == NULL )
+return;
+ while ((c = fgetc(f1))!=EOF)
+{
 
+    if (c=='"')
+    {
+    s++;
+    if (s%2!=0)
+    SetConsoleTextAttribute(hConsole, 11);
+    if (s%2==0)
+    {
+        printf("%c",c);
+        SetConsoleTextAttribute(hConsole, 15);
+        continue;
+    }
+    }
+     printf("%c",c);
+}
+fclose(f1);
+}
